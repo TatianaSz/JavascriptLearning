0ec403083d16daf31483820e4716598275152e09
Tatiana <Tatiszulik777@gmail.com>
2021-04-26 17:26:30 +0200

    actually 7 methods

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -1,5 +1,5 @@
 let divs;
-for(let i=0; i<5; i++){
+for(let i=0; i<3; i++){
 	divs = document.createElement("div");
 	divs.innerHTML = i;
 	document.body.appendChild(divs);
@@ -11,10 +11,12 @@ let arrayLike = document.getElementsByTagName("div")
 const real1 = Array.from(arrayLike);
 real1.forEach((element, index) =>{console.log("I am an Array element!" + " " + index)});
 
-let j =0;
+let real2 = [];
+let j=0;
 for(const el of arrayLike){
-	console.log("I am also Array element!" + " " + j++)
+	real2.push(el);
 }
+real2.forEach((element, index) => console.log("I am also Array element! " + j++));
 
 let real3 = [...arrayLike];
 real3.forEach((element, index) => console.log("An array here too! " + index))
@@ -27,3 +29,13 @@ real5.forEach((element, index) => console.log("Array again " + index))
 
 let real6 = Array.prototype.slice.call(arrayLike);
 real6.forEach((element, index) => console.log("You guessed, array :) " + index))
+
+function realFunction() {
+    var real7 = [];
+    for (var i = 0; i < arrayLike.length; i++) {
+        real7.push(arrayLike[i]);
+	
+    }
+	real7.forEach((element, index) => console.log("Yay, array " + index));
+}
+realFunction(arrayLike);
