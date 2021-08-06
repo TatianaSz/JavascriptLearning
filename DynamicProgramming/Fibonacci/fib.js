8c7b564de6f852e913c1aba627006160d100fef8
Tatiana <Tatiszulik777@gmail.com>
2021-08-06 20:53:59 +0200

    another function

--- DynamicProgramming/Fibonacci/fib.js
+++ DynamicProgramming/Fibonacci/fib.js
@@ -1,9 +1,28 @@
 const fib = function(n, save={}){
     if(n<=2)return 1;
+ 
     if(n in save){return save[n] };
     save[n] = fib(n-1, save) + fib(n-2, save);
+    
     return save[n];
 }
-console.log(fib(50));
+//console.log(fib(4));
 
 
+const Sum=(t,num, memo={})=>{
+    if(t===0) return [];
+    if(t in memo)return memo[t]
+    if(t<0) return null;
+
+    for(let n of num){
+        let s=t-n;
+        const rem = Sum(s,num,memo);
+        if(rem!==null){
+           memo[t]=[...rem,n];
+           return memo[t];
+        }
+    }
+    memo[t] =null;
+    return null
+}
+console.log(Sum(7000,[3,3]))
